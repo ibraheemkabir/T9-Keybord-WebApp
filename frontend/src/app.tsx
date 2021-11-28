d986471dd228bb544e2661482594c5bf64288d34
ibraheemkabir <ibraheemkabir96@gmail.com>
2021-11-28 01:49:35 +0000

    finshed initial react and webpack initialisation for frontend

---
+++ frontend/src/app.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export function App(){
+    return (
+        <div className="page_layout">
+           app index
+        </div>
+    )
+}
---
+++ frontend/webpack.config.js
@@ -0,0 +1,56 @@
+const prod = process.env.NODE_ENV === 'production';
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: prod ? 'production' : 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: __dirname + '/dist/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: ['.ts', '.tsx', '.js', '.json'],
+        },
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+            {
+                loader: 'style-loader',
+            },
+            {
+                loader: 'css-loader',
+                options: {
+                    sourceMap: true,
+                },
+            },
+            {
+                loader: 'sass-loader',
+                options: {
+                    sourceMap: true,
+                },
+            },
+        ]
+      }
+    ]
+  },
+  devtool: prod ? undefined : 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
