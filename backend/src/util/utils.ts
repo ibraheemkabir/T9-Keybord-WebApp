a62353ce16b4ed47047339705ef601bea387caf6
ibraheemkabir <ibraheemkabir96@gmail.com>
2021-11-30 20:02:47 +0000

    initial t9 implementation done with word mapping

---
+++ backend/src/util/utils.ts
@@ -0,0 +1,16 @@
+
+import fs from 'fs';
+import wordListPath from 'word-list';
+ 
+export const wordArray = fs.readFileSync(wordListPath, 'utf8').split('\n');
+
+export const numbersMap :{ [k:string] : string[] } = {
+    "2": ['a','b','c'],
+    "3": ['d','e','f'],
+    "4": ['g','h','i'],
+    "5": ['j','k','l'],
+    "6": ['m','n','o'],
+    "7": ['p','q','r','s'],
+    "8": ['t','u','v'],
+    "9": ['w','x','y','z'],
+}
